971f9ae11fe388a185c35691dfe9c9229cc30b45
Oded Katz <Oded.Katz@enghouse.com>
2020-11-12 18:00:54 +0000

    Add CreateCertifiateLogin.sh

--- CreateCertifiateLogin.sh
+++ CreateCertifiateLogin.sh
@@ -1,8 +1,9 @@
 
 export USERNAME=${1}
 export HOST=${2}
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
+if [ ! -f ~/.ssh/id_rsa.pub  ]; then
     ssh-keygen;
 fi
+
 
 cat ~/.ssh/id_rsa.pub | ssh ${USERNAME}@${HOST} "cat >>~/.ssh/authorized_keys"
