7580aa598498e61476dcff1be7b897e08aaba77d
Oded Katz <Oded.Katz@enghouse.com>
2020-12-24 20:20:56 +0000

    updated RemoveCSSourceDirCompiledFiles.sh

--- RemoveCSSourceDirCompiledFiles.sh
+++ RemoveCSSourceDirCompiledFiles.sh
@@ -1,3 +1,6 @@
+export SRC_DIR=${1}
+
+cd ${SRC_DIR};
 git status -s --ignored | grep "\!\!" | grep "\.o" | /home/okatz/scripts/ReplaceFileList.sh ".* " | xargs rm -f
 
 git status -s --ignored | grep "??" | grep "lexer" | /home/okatz/scripts/ReplaceFileList.sh ".* " | xargs rm -f
@@ -8,13 +11,13 @@ git status -s --ignored | grep "??" | grep "grammar" | /home/okatz/scripts/Repla
 
 
 
-cd ../okatz_5.11.0.bicc.il.build/
+cd ${SRC_DIR}.build;
 
 
 
 if [ "$(uname)" == "SunOS" ];    then	
 
-    ../okatz_5.11.0.bicc.il/configure CC="gcc -m64" CXX="g++ -m64" CFLAGS="" CXXFLAGS="" CPPFLAGS="" LDFLAGS="-L/usr/sfw/lib/amd64 -R/usr/sfw/lib/amd64 -L/usr/lib/amd64 -R/usr/lib/amd64"
+    ../${SRC_DIR}/configure CC="gcc -m64" CXX="g++ -m64" CFLAGS="" CXXFLAGS="" CPPFLAGS="" LDFLAGS="-L/usr/sfw/lib/amd64 -R/usr/sfw/lib/amd64 -L/usr/lib/amd64 -R/usr/lib/amd64"
 
 fi
 
@@ -22,7 +25,7 @@ fi
 
 if [ "$(uname)" == "Linux" ];    then	
 
-    ../okatz_5.11.0.bicc.il/configure 'CC=gcc -m64' 'CXX=g++ -m64'
+    ../${SRC_DIR}/configure 'CC=gcc -m64' 'CXX=g++ -m64'
 
 fi
 
