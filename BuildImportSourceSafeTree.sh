06bed22618174f534d18381e63e29623b17e8af3
Oded Katz <Oded.Katz@enghouse.com>
2021-01-19 22:38:07 +0000

    SS Import

--- BuildImportSourceSafeTree.sh
+++ BuildImportSourceSafeTree.sh
@@ -5,5 +5,7 @@ export GIT_REPOSITORY_PATH=${3}
 export  SSDIR=\\\\Newton\\Archive\\${SOURCESAFE_DB}
 export SOURCESAFE_SUB_TREE_SLASHED=$(echo ${SOURCESAFE_SUB_TREE} | ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "/" "//")
 
+echo ${SOURCESAFE_SUB_TREE_SLASHED}
+
 ss Dir ${SOURCESAFE_SUB_TREE}/ -F- -Yokatz,Johanson23 | grep -v item | grep -v : \
-| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "$\(.*\)\r" "ss Dir ${SOURCESAFE_SUB_TREE}/\1 -F- -Yokatz,Johanson23 | grep -v item | grep -v : | ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh '$\\\\(.*\\\\)\\\\r' './ImportSourceSafeIntoGit.sh ${SOURCESAFE_DB} \1 \\\\1 '"    |sh
+| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "$\(.*\)\r" "ss Dir ${SOURCESAFE_SUB_TREE}/\1 -F- -Yokatz,Johanson23 | grep -v item | grep -v : | ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh '$\\\\(.*\\\\)\\\\r' './ImportSourceSafeIntoGit.sh ${SOURCESAFE_DB} ${SOURCESAFE_SUB_TREE_SLASHED}\1 \\\\1 '"    |sh
