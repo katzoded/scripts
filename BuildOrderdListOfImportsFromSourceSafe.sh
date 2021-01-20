dc1dc141a47b479de68638c3d4c88e7557be4bd5
Oded Katz <Oded.Katz@dialogic.com>
2021-01-20 01:18:28 +0000

    Complete SS Import scripts

---
+++ BuildOrderdListOfImportsFromSourceSafe.sh
@@ -0,0 +1,18 @@
+export SOURCESAFE_HISTORY_DATAFILE=${1}
+export GIT_REPOSITORY_PATH=${2}
+
+
+cat ${SOURCESAFE_HISTORY_DATAFILE} \
+| grep -v "Label:" \
+| grep -v Share \
+| grep -v SHARE \
+| grep -v "is not an existing filename or project" \
+| grep -v "Building list for"> ${SOURCESAFE_HISTORY_DATAFILE}.clean
+
+echo "mkdir ${GIT_REPOSITORY_PATH}; cd ${GIT_REPOSITORY_PATH}; git init;" > DoCommands.sh
+
+cat ${SOURCESAFE_HISTORY_DATAFILE}.clean \
+| awk  '{print "\""$1" "$3"\" "$5" "$6" "$7" "$4}' \
+| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "\([0-9]*\)/\([0-9]*\)/\([0-9]*\)\ " "20\3 \2 \1 " \
+| awk '{ printf "%s/%02d/%02d %s %s %s %s %s\n", $1, $2, $3, $4, $5, $6, $7, $8 }' |sort \
+| awk  '{print "~/dev-newton/scripts/DoImportSourceSafeFilesAndCommit.sh "$3" "$4" "$5" "$6" ${GIT_REPOSITORY_PATH}" }' >> DoCommands.sh
--- ImportSourceSafeIntoGit.sh
+++ ImportSourceSafeIntoGit.sh
@@ -6,8 +6,8 @@ export GIT_REPOSITORY_PATH=${4}
 export  SSDIR=\\\\Newton\\Archive\\${SOURCESAFE_DB}
 
 rm -f hist.log;
-ss History  -L -ohist.log -Yokatz,Johanson23 \$/${SOURCESAFE_SUB_TREE}/${SOURCESAFE_PROJECT};
+ss History  -L -ohist.log -Yokatz,Johanson23 ${SOURCESAFE_SUB_TREE}/${SOURCESAFE_PROJECT};
 cat hist.log |  tr '\n' ' ' | tr '\r' ' ' \
-| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "Label:" "Label:\n" \
-| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "\(\".*\"\).*Date:\(.*\) Time:\(.*\) Labeled.*" "\2:\3 \1 ${SOURCESAFE_DB} ${SOURCESAFE_SUB_TREE} ${SOURCESAFE_PROJECT}" >> ImportSS.Data
+| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "Label:" "\nLabel:" | grep -v "History of" \
+| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "Label: \(\".*\"\).*Date:\(.*\) Time:\(.*\) Labeled.*" "\2:\3 \1 ${SOURCESAFE_DB} ${SOURCESAFE_SUB_TREE} ${SOURCESAFE_PROJECT}" >> ImportSS.Data
 
