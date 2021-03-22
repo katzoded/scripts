b8fc40f4a19797811b0959cebbc9367b0d53c60c
Oded Katz <Oded.Katz@enghouse.com>
2021-03-22 19:15:32 +0100

    adding ExtractBnetscsFromInstallFile.sh

--- ExtractBnetscsFromInstallFile.sh
+++ ExtractBnetscsFromInstallFile.sh
@@ -5,4 +5,4 @@ export BNET_INSTALL_FILE=${1}
 tar tvf ${BNET_INSTALL_FILE} \
 | grep "rpm" \
 | awk '{print $6}' \
-| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "\(.*\)" "tar xvf ${BNET_INSTALL_FILE} \1; rpm2cpio \1 | cpio -tv | grep 'bin/bnetscs' | awk '{print \\\\$9}' | ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh '\\\\(.*\\\\)' 'rpm2cpio \1 | cpio -idmv \\\\1'"
+| ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh "\(.*\)" "tar xvf ${BNET_INSTALL_FILE} \1; rpm2cpio \1 | cpio -tv | grep 'bin/bnetscs' | awk '{print \\\\$9}' | ~/dev-newton/scripts/NoFileCreationReplaceFileList.sh '\\\\(.*\\\\)' 'rpm2cpio \1 | cpio -idmv \\\\1'" | sh | sh
