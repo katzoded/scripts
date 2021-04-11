ba691bfb4323357f530697f3e82aea581c6c84b2
Oded Katz <Oded.Katz@enghouse.com>
2021-04-11 11:47:20 +0100

    GroupBy script

--- GroupByLog.sh
+++ GroupByLog.sh
@@ -4,5 +4,5 @@
 | cat -A \
 | sort \
 | ~/dev-newton/scripts/ReplaceFileList.sh "0x[0x|0-9|a-f|A-F]*" "" \
-| ~/dev-newton/scripts/ReplaceFileList.sh "\([\ :,\.=\-]\)[0-9]*" "\1" | ~/dev-newton/scripts/ReplaceFileList.sh " Duplicate=, In=sec," "" | sort |uniq -c | sort -r
+| ~/dev-newton/scripts/ReplaceFileList.sh "\([\ :,\.=-\+#]\)[0-9]*" "\1" | ~/dev-newton/scripts/ReplaceFileList.sh " Duplicate=, In=sec," "" | sort |uniq -c | sort -r
 
