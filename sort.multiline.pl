9842511579a09288214ac90e1c905bb86124f0bd
Oded Katz <Oded.Katz@enghouse.com>
2021-04-11 11:46:14 +0100

    multiline scripts

--- sort.multiline.pl
+++ sort.multiline.pl
@@ -8,7 +8,7 @@ use warnings;
 # line, assigning to a file handle INFILE (it is customary to choose
 # all-caps names for file handles in Perl); file handles do not have any
 # prefixing punctuation
-my $keyVal = 0;
+my $keyVal = "";
 my $line;
 my $key;
 my $cmi="";
@@ -18,16 +18,16 @@ my $StartCounting=0;
 my $CloseBracketCurrCount;
 my $OpenBracketCurrCount;
 my $PassesUserFilter=0;
-my $UserFilter="";
-my $UserFilterFileName;
 my $FoundCount = 0;
 my $InMultilineSeparator = $ARGV[0];
 my $InMultilineEndSeparator="";
-my $InGrepFilter = $ARGV[1];
-my $InGrepVFilter=0;
+my $InSortRegExp = $ARGV[1];
 my $InFileName = $ARGV[2];
-
+my $Uniq=0;
+my $PrintCount=0;
+my $HumanSort=0;
 my $new_file = "";
+my $fh;
 my %hash = ();
 
 sub LoadParams
@@ -48,7 +48,7 @@ sub LoadParams
     if($NewIf == 1)
     {
         $InMultilineSeparator = "";
-        $InGrepFilter = "";
+        $InSortRegExp = "";
         $InFileName = "";
 
         for(my $i = 0; $i < $#ARGV; $i++)
@@ -61,18 +61,25 @@ sub LoadParams
             {
                 $InMultilineEndSeparator = $ARGV[$i + 1];
             }
-            elsif($ARGV[$i] eq "-gv")
+            elsif($ARGV[$i] eq "-sp")
             {
-                $InGrepFilter = $ARGV[$i + 1];
-                $InGrepVFilter=1;
-            }
-            elsif($ARGV[$i] eq "-g")
-            {
-                $InGrepFilter = $ARGV[$i + 1];
+                $InSortRegExp = $ARGV[$i + 1];
             }
             elsif($ARGV[$i] eq "-f")
             {
                 $InFileName = $ARGV[$i + 1];
+            }
+            elsif($ARGV[$i] eq "-u")
+            {
+                $Uniq = 1;
+            }
+            elsif($ARGV[$i] eq "-c")
+            {
+                $PrintCount = 1;
+            }
+            elsif($ARGV[$i] eq "-h")
+            {
+                $HumanSort = 1;
             }
         }
     }
@@ -87,10 +94,11 @@ sub printHelp
     print STDOUT "\n-h Print this help";
     print STDOUT "\n-ss \"RegEx for Start Separator\"";
     print STDOUT "\n-es \"RegEx for End Separator\" (in case -es doesn't exist only -ss will be used)";
-    print STDOUT "\n-g \"RegEx for grepping the multiline\" whatever passes this RegEx will be in output";
-    print STDOUT "\n-gv \"RegEx for grepping the multiline\" whatever passes this RegEx will NOT be in output";
+    print STDOUT "\n-sp \"RegEx for sort param\" whatever passes this RegEx will be in output";
     print STDOUT "\n-f \"Input file name\" - this param is not mandatory, STDIN can be used as input ";
-    print STDOUT "\nSupport backward comp of using \"RegExp for Start Separator\" \"RegEx for grepping the multiline\" \"Input File name\"";
+    print STDOUT "\n-u uniq";
+    print STDOUT "\n-c count";
+    print STDOUT "\n-h human sort";
     exit;
 }
 
@@ -105,13 +113,37 @@ sub MyReadline
         $line = <STDIN>;
     }
 }
+
+sub PrintHashEntry
+{
+    my $key=shift;
+    my $PrintedStr="";
+    
+    if($PrintCount == 1)
+    {
+        $PrintedStr = $hash{$key}->{count}." ";
+    }
+    $PrintedStr = $PrintedStr . $hash{$key}->{stack};
+    
+    if($fh)
+    {
+        print $fh $PrintedStr;
+    }
+    print STDOUT $PrintedStr;
+}
+
 sub CheckAndAddToResult
 {
-    if(($PassesUserFilter == 1 && $InGrepVFilter == 0) ||  ($PassesUserFilter == 0 && $InGrepVFilter == 1) )
+    if($PassesUserFilter == 1)
     {
-        $keyVal=$keyVal+1;
-        $hash{$keyVal}->{stack} = $cmi;
+        if($Uniq == 0 || not defined $hash{$keyVal}  )
+        {
+            $hash{$keyVal}->{stack} .= $cmi;
+            $hash{$keyVal}->{count} = 0;
+        }
+        $hash{$keyVal}->{count} = $hash{$keyVal}->{count} + 1;
     }
+    $keyVal="";
     $cmi="";
     $PassesUserFilter=0;
 }
@@ -120,34 +152,8 @@ LoadParams();
 if($InFileName)
 {
     open(INFILE,$InFileName);
-    $new_file = $InFileName;
 }
-if($InGrepFilter)
-{
-	$UserFilter=$InGrepFilter;
-	$UserFilterFileName=$InGrepFilter;
-	$UserFilterFileName =~ s/\{//g;
-	$UserFilterFileName =~ s/\}//g;
-	$UserFilterFileName =~ s/://g;
-	$UserFilterFileName =~ s/ //g;
-	$UserFilterFileName =~ s/\|/-/g;
-	$UserFilter =~ s/\{/\\\{/g;
-	$UserFilter =~ s/\}/\\\}/g;
-	
-#	print STDOUT "Argv=$InGrepFilter, UserFilter=$UserFilter, UserFilterFileName=$UserFilterFileName\n";
 
-    if($InFileName)
-    {
-        $new_file .= ".$UserFilterFileName";
-    }
-}
-else
-{
-    if($InFileName)
-    {
-        $new_file .= ".cmi";
-    }
-}
 MyReadline();
 while ($line)
 {
@@ -163,8 +169,14 @@ while ($line)
             $FoundCount=$FoundCount+@splitarr;
         }
     }
-    if($line =~ /$UserFilter/)
+    if($line =~ /($InSortRegExp)/)
     {
+        my $SortStr=$1;
+        if(defined $2)
+        {
+            $SortStr=$2;
+        }
+        $keyVal=$keyVal.$SortStr; 
         $PassesUserFilter=1;
     }
     $cmi.=$line;
@@ -186,18 +198,23 @@ while ($line)
 
 CheckAndAddToResult();
 
-my $fh;
 if($new_file)
 {
     open($fh, '>', $new_file) or die "Could not open file '$new_file' $!";
 }
-foreach my $key (sort { $a <=> $b } keys %hash)
+if($HumanSort == 0)
 {
-    if($fh)
+    foreach my $key (sort keys %hash)
     {
-        print $fh $hash{$key}->{stack};
+        PrintHashEntry($key);
     }
-    print STDOUT $hash{$key}->{stack};
+}
+else
+{
+    foreach my $key (sort { $a <=> $b } keys %hash)
+    {
+        PrintHashEntry($key);
+    }    
 }
 if($fh)
 {
