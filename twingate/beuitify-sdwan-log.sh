dc15f3badcbea19eeed0077c8d4f7a4c1f0bbf3e
Oded Katz <katz.oded@gmail.com>
2023-04-14 15:07:25 -0700

    add title

--- twingate/beuitify-sdwan-log.sh
+++ twingate/beuitify-sdwan-log.sh
@@ -3,6 +3,7 @@ FILENAME=$1
 echo "#!$(which bash)" > /tmp/beuitify_all_json.sh
 echo "cat - \\" >> /tmp/beuitify_all_json.sh
 
+# build loop for json beuitifier
 cat  ${FILENAME} \
 | grep -e "\{.*\}" \
 | ~/dev-newton/scripts/search_and_replace.py --search "^\[.*[0-9]\] " --replace "" \
@@ -14,12 +15,13 @@ echo "" >> /tmp/beuitify_all_json.sh
 chmod +x /tmp/beuitify_all_json.sh
 
 
-cat $FILENAME \
-| grep -e "\{.*\}" -e "http\:\:re" \
+(echo "title $(basename ${FILENAME})" && cat ${FILENAME}) \
+| grep -e "\{.*\}" -e "http\:\:re" -e "title" \
 | grep -v "http\:\:response\:\:from\: certificate" \
 | /tmp/beuitify_all_json.sh \
 | ~/dev-newton/scripts/search_and_replace.py --search "^(\[.*\]) http\:\:request\:\:send_request\: ([A-Z]*) \"https://(.*)\.com/(.*)\" .*" --replace "sdwan->\3: \2 \4\\\\n \1" \
 | ~/dev-newton/scripts/search_and_replace.py --search "^(\[.*\]) http\:\:request\:\:handle_response\: [A-Z]* \"https://(.*)\.com/.*\" (.*) \(.*\)" --replace "\2->sdwan:\3: \\\\n \1"
+
 
 rm -f /tmp/beuitify_all_json.sh
 
